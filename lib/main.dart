9aeafffc87c8ddd2ea721fa6b31d113d559daf1e
Samme Qandil <sqandil@cornellpump.com>
2020-03-03 14:14:35 -0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+// Author Samme Qandil
+import 'package:flutter/material.dart';
+
+import 'ui/home.dart';
+//void main() => runApp(ScaffoldExample());
+
+void main() => runApp(new MaterialApp(
+  home: MovieListView(),
+));
